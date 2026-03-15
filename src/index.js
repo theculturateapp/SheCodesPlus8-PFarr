45a0558164315e9fe569964bcf92f57986562bf4
theculturateapp <theculturateapp@gmail.com>
2026-03-15 13:47:30 -0400

    Daily Forecast Update
    Updated daily forecast update so now the current date and all weather information updates.

--- src/index.js
+++ src/index.js
@@ -54,6 +54,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast (city) {
   let apiKey = "37674b4ao471bb32b44ta37b064ef2da";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
@@ -66,21 +73,27 @@ function displayForecast(response) {
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-days.forEach(function (day) {
-  forecastHtml =
-    forecastHtml +
-    `
+response.data.daily.forEach(function (day, index) {
+  if (index < 5) {
+    forecastHtml =
+      forecastHtml +
+      `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div class="weather-forecast-icon"><img src="${
+          day.condition.icon_url
+        }"></div>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.minimum)}</strong>
           </div>
-          <div class="weather-forecast-temperature">9º</div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.maximum
+          )}</div>
         </div>
       </div>
     `;
+  }
 });
 
   let forecastElement = document.querySelector("#forecast");
